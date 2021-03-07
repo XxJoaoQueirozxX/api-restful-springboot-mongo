86b0fb84653cee7d9b28465b86217a543a948d9c
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-07 16:44:39 -0300

    Projecao dos dados do author com DTO

--- src/main/java/com/restful/api/config/Instantiation.java
+++ src/main/java/com/restful/api/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.restful.api.config;
 
 import com.restful.api.domain.Post;
 import com.restful.api.domain.User;
+import com.restful.api.dto.AuthorDTO;
 import com.restful.api.repositories.PostRepository;
 import com.restful.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,10 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post p1 = new Post(null, LocalDate.parse("21/03/2018", df), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post p2 = new Post(null, LocalDate.parse("21/03/2018", df), "Bom dia", "Acordei feliz hoje!", maria);
+        Post p1 = new Post(null, LocalDate.parse("21/03/2018", df), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post p2 = new Post(null, LocalDate.parse("21/03/2018", df), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
---
+++ src/main/java/com/restful/api/dto/AuthorDTO.java
@@ -0,0 +1,37 @@
+package com.restful.api.dto;
+
+import com.restful.api.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {
+    }
+
+    public AuthorDTO(User user) {
+        this.id = user.getId();;
+        this.name = user.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
