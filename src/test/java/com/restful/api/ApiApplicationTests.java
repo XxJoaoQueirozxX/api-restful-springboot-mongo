344706166e9a76a10d69f972557bd663060279bf
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-07 13:45:08 -0300

    Primeiro commit - pojeto criado

---
+++ src/test/java/com/restful/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
