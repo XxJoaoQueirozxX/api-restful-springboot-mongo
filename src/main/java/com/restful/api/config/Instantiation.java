97cd297f3edd5c43ca454b2919b700b2b406e79d
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-07 16:52:33 -0300

    Referenciando os posts do usuario

--- src/main/java/com/restful/api/config/Instantiation.java
+++ src/main/java/com/restful/api/config/Instantiation.java
@@ -39,5 +39,8 @@ public class Instantiation implements CommandLineRunner {
         Post p1 = new Post(null, LocalDate.parse("21/03/2018", df), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post p2 = new Post(null, LocalDate.parse("21/03/2018", df), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(p1, p2));
+
+        maria.getPosts().addAll(Arrays.asList(p1, p2));
+        userRepository.save(maria);
     }
 }
--- src/main/java/com/restful/api/domain/User.java
+++ src/main/java/com/restful/api/domain/User.java
@@ -1,9 +1,12 @@
 package com.restful.api.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document("user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(String id, String name, String email) {
         this.id = id;
@@ -47,6 +53,11 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
 
     @Override
     public boolean equals(Object o) {
