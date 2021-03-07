bbb8db4c800782037f7e10adbf87dbe82c4352d3
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-07 17:06:20 -0300

    Obtendo post por id

---
+++ src/main/java/com/restful/api/services/PostService.java
@@ -0,0 +1,26 @@
+package com.restful.api.services;
+
+import com.restful.api.domain.Post;
+import com.restful.api.repositories.PostRepository;
+import com.restful.api.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+
+
+    public Post findById(String id){
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+
+
+}
