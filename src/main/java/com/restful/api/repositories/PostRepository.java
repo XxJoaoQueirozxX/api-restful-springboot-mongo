0c38cf6be963186d5da4a7852f2c71a957badf57
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-07 17:44:08 -0300

    Consulta simples com @Query

--- src/main/java/com/restful/api/repositories/PostRepository.java
+++ src/main/java/com/restful/api/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.restful.api.repositories;
 
 import com.restful.api.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,11 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+
+
 }
