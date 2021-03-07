31c9718f42369e78c3c614c6564a9db48d8fbd26
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-03-07 15:30:49 -0300

    Insercao de usuario com POST

--- src/main/java/com/restful/api/resources/UserResource.java
+++ src/main/java/com/restful/api/resources/UserResource.java
@@ -4,14 +4,12 @@ import com.restful.api.domain.User;
 import com.restful.api.dto.UserDTO;
 import com.restful.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,5 +31,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         User u = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(u));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
+        User user = service.fromDTO(userDTO);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/restful/api/services/UserService.java
+++ src/main/java/com/restful/api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.restful.api.services;
 
 import com.restful.api.domain.User;
+import com.restful.api.dto.UserDTO;
 import com.restful.api.repositories.UserRepository;
 import com.restful.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,13 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public User insert(User u){
+        return repository.insert(u);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
 }
